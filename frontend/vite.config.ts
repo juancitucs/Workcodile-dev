ce54c2deedae35faf96cd4ef632bba4ccd174f28
juancitucs <juancitolinuxlover@gmail.com>
2025-11-14 19:37:01 -0500

    FIX: Netlify browser xdg-open

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true,
+    open: false,
     allowedHosts: ['stem-edmonton-dose-discipline.trycloudflare.com'],
   },
 })
