6996b82b5f471c061a0ab8edbc38272452874041
juancitucs <juancitolinuxlover@gmail.com>
2025-12-24 18:51:43 -0500

    FIX: hide unauthenticated users 2 (public endpoint through middleware)

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -6,6 +6,6 @@ const userController = require('../controllers/userController');
 // @route   GET api/users/top
 // @desc    Get top users by experience points (XP)
 // @access  Private
-router.get('/top', authMiddleware, userController.getTopUsers);
+router.get('/top', userController.getTopUsers);
 
 module.exports = router;
